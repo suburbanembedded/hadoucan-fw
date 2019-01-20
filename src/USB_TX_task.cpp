78d7f4bb5d90abdd7f4df83ebd3ea77242205671
Jacob Schloss <jacob@schloss.io>
2019-01-19 16:30:21 -0800

    cleanup

--- src/USB_TX_task.cpp
+++ src/USB_TX_task.cpp
@@ -32,13 +32,13 @@ void USB_TX_task::work()
 		// uart1_print<64>("tx wait buf\r\n");
 
 		USB_buf* usb_buffer = nullptr;
-		if(!m_pending_tx_buffers.pop_front(&usb_buffer, pdMS_TO_TICKS(50)))
+		if(!m_pending_tx_buffers.pop_front(&usb_buffer, pdMS_TO_TICKS(USB_HS_PACKET_WAIT_MS)))
 		{
 			//if we previously sent data, and don't have more to send we should send a null packet
 			//this is a hint to the OS to flush buffers / hand data to the application
 			if(m_needs_send_null)
 			{
-				// uart1_log<128>(LOG_LEVEL::INFO, "USB_TX_task", "tx idle for 50ms, send null buf");
+				uart1_log<128>(LOG_LEVEL::INFO, "USB_TX_task", "tx idle for 50ms, send null buf");
 				send_buffer(nullptr);
 				m_needs_send_null = false;
 			}
--- src/USB_tx_buffer_task.hpp
+++ src/USB_tx_buffer_task.hpp
@@ -10,15 +10,13 @@
 #include <deque>
 #include <vector>
 
-//aggragate small writes
+//aggregate small writes
 class USB_tx_buffer_task : public Task_static<1024>
 {
 public:
 
-  //if an insertion would put us over this limit
+  //if an insertion would put us over this limit, block until there is space
   const size_t BUFFER_HIGH_WATERMARK = 1024 * 16;
-  //block until it is below this limit
-  const size_t BUFFER_LOW_WATERMARK = 1024 * 12;
 
 	USB_tx_buffer_task()
 	{
@@ -65,11 +63,13 @@ protected:
 
 	//insert [first, last) into the internal buffer
 	//len is used to cap ram usage
+	//will block until space is available
 	template<typename InputIt>
 	void write(InputIt first, InputIt last, const size_t len)
 	{
 		std::function<bool()> enqueue_pred = std::bind(&USB_tx_buffer_task::m_tx_buf_has_space, this, len);
 
+		//wait until m_tx_buf_has_space says we can insert our stuff
 		{
 			std::unique_lock<Mutex_static> lock(m_tx_buf_mutex);
 			
@@ -84,7 +84,7 @@ protected:
 
 	//predicate for enqueue to m_tx_buf_condvar
 	//m_tx_buf_mutex must be locked
-	bool m_tx_buf_has_space(const size_t len)
+	bool m_tx_buf_has_space(const size_t len) const
 	{
 		//false if keep waiting
 		return (m_tx_buf.size() + len) < BUFFER_HIGH_WATERMARK;
@@ -92,7 +92,7 @@ protected:
 
 	//predicate for m_tx_buf_condvar write to usb
 	//m_tx_buf_mutex must be locked
-	bool has_buffer()
+	bool has_buffer() const
 	{
 		if(m_tx_buf.empty())
 		{
@@ -101,7 +101,7 @@ protected:
 			return false;
 		}
 
-		if(m_tx_buf.size() >= 512)
+		if(m_tx_buf.size() >= USB_HS_PACKET_MAX_LEN)
 		{
 			//yay, full USB HS packet
 			return true;
@@ -110,13 +110,13 @@ protected:
 		return false;
 	}
 
+	static const uint32_t USB_HS_PACKET_WAIT_MS = 50;
+	static const uint32_t USB_HS_PACKET_MAX_LEN = 512;
+
 	std::deque<uint8_t> m_tx_buf;
 	Mutex_static m_tx_buf_mutex;
 	Condition_variable m_tx_buf_condvar;
 	Condition_variable m_tx_buf_drain_condvar;
-
-	TimeOut_t  m_tx_timeout;
-	TickType_t m_tx_timeout_ticks_left;
 
 	USB_TX_task* m_usb_tx_task;
 };
