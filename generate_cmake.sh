189b9de45e3e0e37eae36166032d4e3e9bfca74f
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2019-02-11 16:21:30 -0800

    add cmake toolchain file, add ram ld script, add ram build config

--- generate_cmake.sh
+++ generate_cmake.sh
@@ -1,9 +1,23 @@
-mkdir -p build/release
-pushd build/release
-cmake -DCMAKE_BUILD_TYPE=Release ../..
+if [ -d build ]; then rm -rf build; fi
+
+BASE_PATH=$PWD
+
+mkdir -p build/ram/release
+pushd build/ram/release
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$BASE_PATH/Toolchain_stm32h750_atollic_ram.cmake $BASE_PATH
 popd
 
-mkdir -p build/debug
-pushd build/debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../..
+mkdir -p build/ram/debug
+pushd build/ram/debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=$BASE_PATH/Toolchain_stm32h750_atollic_ram.cmake $BASE_PATH
+popd
+
+mkdir -p build/flash/release
+pushd build/flash/release
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$BASE_PATH/Toolchain_stm32h750_atollic_flash.cmake $BASE_PATH
+popd
+
+mkdir -p build/flash/debug
+pushd build/flash/debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=$BASE_PATH/Toolchain_stm32h750_atollic_flash.cmake $BASE_PATH
 popd
