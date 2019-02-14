d7f6e3c53e61bcec62ec4c64ba97f3bfccead32a
Jacob Schloss <jacob@schloss.io>
2019-02-13 20:57:04 -0800

    change led

--- src/main.cpp
+++ src/main.cpp
@@ -255,17 +255,17 @@ public:
 			vTaskDelay(250);
 			HAL_GPIO_WritePin(GPIOD, RED1_Pin, GPIO_PIN_SET);
 
-			HAL_GPIO_WritePin(GPIOD, RED2_Pin, GPIO_PIN_SET);
-			vTaskDelay(250);
-			HAL_GPIO_WritePin(GPIOD, RED2_Pin, GPIO_PIN_RESET);
-			vTaskDelay(250);
-			HAL_GPIO_WritePin(GPIOD, RED2_Pin, GPIO_PIN_SET);
-
 			HAL_GPIO_WritePin(GPIOD, GREEN1_Pin, GPIO_PIN_SET);
 			vTaskDelay(250);
 			HAL_GPIO_WritePin(GPIOD, GREEN1_Pin, GPIO_PIN_RESET);
 			vTaskDelay(250);
 			HAL_GPIO_WritePin(GPIOD, GREEN1_Pin, GPIO_PIN_SET);
+
+			HAL_GPIO_WritePin(GPIOD, RED2_Pin, GPIO_PIN_SET);
+			vTaskDelay(250);
+			HAL_GPIO_WritePin(GPIOD, RED2_Pin, GPIO_PIN_RESET);
+			vTaskDelay(250);
+			HAL_GPIO_WritePin(GPIOD, RED2_Pin, GPIO_PIN_SET);
 
 			HAL_GPIO_WritePin(GPIOD, GREEN2_Pin, GPIO_PIN_SET);
 			vTaskDelay(250);
