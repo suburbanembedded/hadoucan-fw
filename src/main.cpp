13918b7856de09134c827b28bf9d74140753123c
Jacob Schloss <jacob@schloss.io>
2019-03-02 21:21:46 -0800

    refactor tim3 config

--- src/main.cpp
+++ src/main.cpp
@@ -206,15 +206,137 @@ public:
 
 	void work() override
 	{
-		HAL_GPIO_WritePin(MASTER_TIMESYNC_nOE_GPIO_Port, MASTER_TIMESYNC_nOE_Pin, GPIO_PIN_RESET);
-
-		HAL_TIM_Base_Start(&htim3);
-		HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
-
+		if(!ic_config())
+		{
+			uart1_log<64>(LOG_LEVEL::ERROR, "Timesync_task", "ic_config failed");
+		}
+		
 		for(;;)
 		{
-			vTaskDelay(250);
+			vTaskSuspend(nullptr);
 		}
+	}
+
+	bool oc_config()
+	{
+		// 100 MHz input clock
+		// 2000 prescaler -> 20us per tick
+		// 50000 counts -> 1s overflow
+
+		htim3.Instance = TIM3;
+		htim3.Init.Prescaler = 2000;
+		htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+		htim3.Init.Period = 50000;
+		htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+		htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+		if(HAL_TIM_Base_Init(&htim3) != HAL_OK)
+		{
+			return false;
+		}
+
+		TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+		sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+		if(HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+		{
+			return false;
+		}
+
+		TIM_MasterConfigTypeDef sMasterConfig = {0};
+		sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+		sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+		if(HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+		{
+			return false;
+		}
+
+		HAL_TIM_Base_Start(&htim3);
+
+		if(HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+		{
+			return false;
+		}
+
+		TIM_OC_InitTypeDef sConfigOC = {0};
+		sConfigOC.OCMode = TIM_OCMODE_PWM1;
+		sConfigOC.Pulse = 5;
+		sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+		sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+		if(HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+		{
+			return false;
+		}
+
+		HAL_GPIO_WritePin(MASTER_TIMESYNC_nOE_GPIO_Port, MASTER_TIMESYNC_nOE_Pin, GPIO_PIN_RESET);
+
+		HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+
+		return true;
+	}
+
+	bool ic_config()
+	{
+		// 100 MHz input clock
+		// 50000 prescaler -> 500us per tick
+		// 50000 counts -> 25s overflow
+
+		htim3.Instance = TIM3;
+		htim3.Init.Prescaler = 50000;
+		htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+		htim3.Init.Period = 50000;
+		htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+		htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+		if(HAL_TIM_Base_Init(&htim3) != HAL_OK)
+		{
+			return false;
+		}
+
+		TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+		sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+		if(HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+		{
+			return false;
+		}
+
+		if(HAL_TIM_IC_Init(&htim3) != HAL_OK)
+		{
+			return false;
+		}
+
+		TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+		sSlaveConfig.SlaveMode = TIM_SLAVEMODE_RESET;
+		sSlaveConfig.InputTrigger = TIM_TS_TI1FP1;
+		sSlaveConfig.TriggerPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+		sSlaveConfig.TriggerFilter = 0;
+		if(HAL_TIM_SlaveConfigSynchronization(&htim3, &sSlaveConfig) != HAL_OK)
+		{
+			return false;
+		}
+
+		TIM_MasterConfigTypeDef sMasterConfig = {0};
+		sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+		sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+		if(HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+		{
+			return false;
+		}
+
+		TIM_IC_InitTypeDef sConfigIC = {0};
+		sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+		sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+		sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+		sConfigIC.ICFilter = 0;
+		if(HAL_TIM_IC_ConfigChannel(&htim3, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+		{
+			return false;
+		}
+
+		HAL_GPIO_WritePin(MASTER_TIMESYNC_nOE_GPIO_Port, MASTER_TIMESYNC_nOE_Pin, GPIO_PIN_SET);
+
+		HAL_TIM_Base_Start(&htim3);
+
+		HAL_TIM_IC_Start(&htim3, TIM_CHANNEL_1);
+
+		return true;
 	}
 };
 Timesync_task timesync_task;
@@ -765,7 +887,6 @@ int main(void)
 	//Enable backup domain in standby and Vbat mode
 	HAL_PWREx_EnableBkUpReg();
 
-
 	{
 		std::array<char, 25> id_str;
 		get_unique_id_str(&id_str);
@@ -780,7 +901,7 @@ int main(void)
 	MX_HASH_Init();
 	MX_RTC_Init();
 	MX_RNG_Init();
-	MX_TIM3_Init();
+	// MX_TIM3_Init();
 	// MX_QUADSPI_Init();
 
 	if(0)
