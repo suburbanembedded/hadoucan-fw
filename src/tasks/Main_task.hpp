98f3b1822b2662300251c4b525814cb5203160d2
Jacob Schloss <jacob@schloss.io>
2019-07-11 11:28:44 -0700

    Refactor tasks

---
+++ src/tasks/Main_task.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "freertos_cpp_util/Task_static.hpp"
+
+#include "CAN_USB_app.hpp"
+
+#include "libusb_dev_cpp/usb_core.hpp"
+#include "libusb_dev_cpp/driver/stm32/stm32_h7xx_otghs.hpp"
+#include "libusb_dev_cpp/util/Descriptor_table.hpp"
+
+class Main_task : public Task_static<4096>
+{
+public:
+	void work() override;
+
+	bool init_usb();
+
+	bool mount_fs();
+
+	bool load_config();
+
+protected:
+	
+	void get_unique_id(std::array<uint32_t, 3>* id);
+	void get_unique_id_str(std::array<char, 25>* id_str);
+
+	Descriptor_table usb_desc_table;
+	std::array<char, 25> usb_id_str;
+};
+
+
+extern USB_core         usb_core;
+extern stm32_h7xx_otghs usb_driver;
+extern EP_buffer_mgr_freertos<3, 4, 512, 32> usb_tx_buffer;
+extern EP_buffer_mgr_freertos<3, 4, 512, 32> usb_rx_buffer;
---
+++ src/tasks/Task_instances.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "LED_task.hpp"
+#include "Main_task.hpp"
+#include "Status_task.hpp"
+#include "STM32_fdcan_rx.hpp"
+#include "Timesync_task.hpp"
+#include "USB_lawicel_task.hpp"
+#include "USB_poll.hpp"
+
+extern USB_rx_buffer_task usb_rx_buffer_task;
+extern USB_tx_buffer_task usb_tx_buffer_task;
+
+extern Test_USB_Core_task test_usb_core;
+
+extern LED_task led_task;
+extern USB_lawicel_task usb_lawicel_task;
+extern Timesync_task timesync_task;
+
+extern Main_task main_task;
