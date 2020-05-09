4cacf094a7a47b22cf42fbed0c4c78b55f32486a
Jacob Schloss <jacob@schloss.io>
2020-05-08 23:59:28 -0700

    run as parent's id

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -6,9 +6,9 @@ set -v
 
 CONTAINER_ID=$(docker create -v $GITHUB_WORKSPACE:/tmp/workspace -it docker.pkg.github.com/suburbanembedded/hadoucan-fw/hadoucan-fw-tools:${GITHUB_REF##*/}  /bin/bash)
 docker start $CONTAINER_ID
-docker exec -u root -w /tmp/workspace/ $CONTAINER_ID ./generate_cmake.sh
-docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/ram/debug/
-docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/ram/release/
+docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID ./generate_cmake.sh
+docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/ram/debug/
+docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/ram/release/
 docker stop $CONTAINER_ID
 
 pushd $GITHUB_WORKSPACE/build/ram/debug
