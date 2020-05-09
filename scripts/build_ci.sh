89fcce0586e74ba83b97e224056d1f0f37fe5a0b
Jacob Schloss <jacob@schloss.io>
2020-05-08 21:34:10 -0700

    make artifact tarball, upload artifacts

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -7,6 +7,9 @@ set -v
 CONTAINER_ID=$(docker create -v $GITHUB_WORKSPACE:/tmp/workspace -it docker.pkg.github.com/suburbanembedded/hadoucan-fw/hadoucan-fw-tools:latest  /bin/bash)
 docker start $CONTAINER_ID
 docker exec -u root -w /tmp/workspace/ $CONTAINER_ID ./generate_cmake.sh
-docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -C build/ram/debug/
-docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -C build/ram/release/
+docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/ram/debug/
+docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/ram/release/
 docker stop $CONTAINER_ID
+
+tar -czf $GITHUB_WORKSPACE/canusbfdiso-debug.tar.gz $GITHUB_WORKSPACE/build/ram/debug/canusbfdiso.elf $GITHUB_WORKSPACE/build/ram/debug/canusbfdiso.hex $GITHUB_WORKSPACE/build/ram/debug/canusbfdiso.bin
+tar -czf $GITHUB_WORKSPACE/canusbfdiso-release.tar.gz $GITHUB_WORKSPACE/build/ram/release/canusbfdiso.elf $GITHUB_WORKSPACE/build/ram/release/canusbfdiso.hex $GITHUB_WORKSPACE/build/ram/release/canusbfdiso.bin
