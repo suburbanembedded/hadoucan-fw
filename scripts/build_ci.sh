2a8d1bec29ebd75f12510c518f4c5a02ef7adf98
Jacob Schloss <jacob@schloss.io>
2020-05-08 18:49:18 -0700

    test

---
+++ scripts/build_ci.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+trap 'exit -1' err
+
+set -v
+
+CONTAINER_ID=$(docker create -it docker.pkg.github.com/suburbanembedded/hadoucan-fw/hadoucan-fw-tools:latest -v $GITHUB_WORKSPACE:/tmp/workspace /bin/bash)
+docker start $CONTAINER_ID
+docker exec -u root -w /tmp/workspace/ $CONTAINER_ID ./generate_cmake.sh
+docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -C build/ram/debug/
+docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -C build/ram/release/
+docker stop $CONTAINER_ID
