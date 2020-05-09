99b8f63bdf2ea7e5e16bb62682102bbb0b32bee2
Jacob Schloss <jacob@schloss.io>
2020-05-08 21:11:42 -0700

    move -v

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -4,7 +4,7 @@ trap 'exit -1' err
 
 set -v
 
-CONTAINER_ID=$(docker create -it docker.pkg.github.com/suburbanembedded/hadoucan-fw/hadoucan-fw-tools:latest -v $GITHUB_WORKSPACE:/tmp/workspace /bin/bash)
+CONTAINER_ID=$(docker create -v $GITHUB_WORKSPACE:/tmp/workspace -it docker.pkg.github.com/suburbanembedded/hadoucan-fw/hadoucan-fw-tools:latest  /bin/bash)
 docker start $CONTAINER_ID
 docker exec -u root -w /tmp/workspace/ $CONTAINER_ID ./generate_cmake.sh
 docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -C build/ram/debug/
