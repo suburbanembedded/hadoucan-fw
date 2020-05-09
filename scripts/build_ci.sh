beefd96b0a56d2563e0f71bf304c06ce287ba452
Jacob Schloss <jacob@schloss.io>
2020-05-08 21:45:43 -0700

    try to have less folders. try per-branch docker

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -4,12 +4,12 @@ trap 'exit -1' err
 
 set -v
 
-CONTAINER_ID=$(docker create -v $GITHUB_WORKSPACE:/tmp/workspace -it docker.pkg.github.com/suburbanembedded/hadoucan-fw/hadoucan-fw-tools:latest  /bin/bash)
+CONTAINER_ID=$(docker create -v $GITHUB_WORKSPACE:/tmp/workspace -it docker.pkg.github.com/suburbanembedded/hadoucan-fw/hadoucan-fw-tools:${GITHUB_REF##*/}  /bin/bash)
 docker start $CONTAINER_ID
 docker exec -u root -w /tmp/workspace/ $CONTAINER_ID ./generate_cmake.sh
 docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/ram/debug/
 docker exec -u root -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/ram/release/
 docker stop $CONTAINER_ID
 
-tar -czf $GITHUB_WORKSPACE/canusbfdiso-debug.tar.gz $GITHUB_WORKSPACE/build/ram/debug/canusbfdiso.elf $GITHUB_WORKSPACE/build/ram/debug/canusbfdiso.hex $GITHUB_WORKSPACE/build/ram/debug/canusbfdiso.bin
-tar -czf $GITHUB_WORKSPACE/canusbfdiso-release.tar.gz $GITHUB_WORKSPACE/build/ram/release/canusbfdiso.elf $GITHUB_WORKSPACE/build/ram/release/canusbfdiso.hex $GITHUB_WORKSPACE/build/ram/release/canusbfdiso.bin
+tar -czf -C $GITHUB_WORKSPACE/build/ram/debug   $GITHUB_WORKSPACE/canusbfdiso-debug.tar.gz   canusbfdiso.elf canusbfdiso.hex canusbfdiso.bin
+tar -czf -C $GITHUB_WORKSPACE/build/ram/release $GITHUB_WORKSPACE/canusbfdiso-release.tar.gz canusbfdiso.elf canusbfdiso.hex canusbfdiso.bin
