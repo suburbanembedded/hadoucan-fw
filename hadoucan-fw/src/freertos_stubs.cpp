78d810cfdaa5f682a33837915753deddc8aba71a
Jacob Schloss <jacob@schloss.io>
2020-05-25 13:10:15 -0700

    version string

--- hadoucan-fw/src/freertos_stubs.cpp
+++ hadoucan-fw/src/freertos_stubs.cpp
@@ -72,7 +72,6 @@ void handle_config_assert(const char* file, const int line, const char* msg)
   Stack_string<128> str;
   str.sprintf("Config assert at %s:%d: %d", file, line, msg);
   {
-    const char msg[] = "Config Assert - ";
     HAL_UART_Transmit(&huart1, reinterpret_cast<uint8_t*>(const_cast<char*>(str.data())), str.size(), -1);
   }
 }
