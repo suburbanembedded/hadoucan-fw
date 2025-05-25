9f82bf792633fe6d3049e83023ebb6ddb4a8250f
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2025-05-25 18:25:17 +0100

    update to tim17

--- hadoucan-fw/src/stm32h7xx_it.c
+++ hadoucan-fw/src/stm32h7xx_it.c
@@ -1,26 +1,94 @@
-#include "stm32h7xx_it.h"
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32h7xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
 
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "hal_inst.h"
+#include "stm32h7xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
 
-#include "cmsis_os.h"
-#include "core_cm7.h"
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
 
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
 extern PCD_HandleTypeDef hpcd_USB_OTG_HS;
+extern FDCAN_HandleTypeDef hfdcan1;
+extern UART_HandleTypeDef huart1;
 extern TIM_HandleTypeDef htim17;
 
-void FDCAN1_IT0_IRQHandler(void);
-void FDCAN2_IT0_IRQHandler(void);
-void FDCAN1_IT1_IRQHandler(void);
-void FDCAN2_IT1_IRQHandler(void);
+/* USER CODE BEGIN EV */
 
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
 void NMI_Handler(void)
 {
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
 
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+   while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
 void HardFault_Handler(void)
 {
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
   // Configurable Fault Status Register
   volatile uint32_t CFSR = SCB->CFSR;
   // Hard Fault Status Register
@@ -35,38 +103,37 @@ void HardFault_Handler(void)
   volatile uint32_t BFAR = SCB->BFAR;
   //System Handler Control and State Register
   volatile uint32_t SHCSR = SCB->SHCSR;
-  
-  for(;;)
-  {
 
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
   }
 }
 
+/**
+  * @brief This function handles Memory management fault.
+  */
 void MemManage_Handler(void)
 {
-  // Configurable Fault Status Register
-  volatile uint32_t CFSR = SCB->CFSR;
-  // Hard Fault Status Register
-  volatile uint32_t HFSR = SCB->HFSR;
-  // Debug Fault Status Register
-  volatile uint32_t DFSR = SCB->DFSR;
-  // Auxiliary Fault Status Register
-  volatile uint32_t AFSR = SCB->AFSR;
-  // MemManage Fault Address Register
-  volatile uint32_t MMFAR = SCB->MMFAR;
-  // BusFault Address Register
-  volatile uint32_t BFAR = SCB->BFAR;
-  //System Handler Control and State Register
-  volatile uint32_t SHCSR = SCB->SHCSR;
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
 
-  for(;;)
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
   {
-
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
   }
 }
 
+/**
+  * @brief This function handles Pre-fetch fault, memory access fault.
+  */
 void BusFault_Handler(void)
 {
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
   // Configurable Fault Status Register
   volatile uint32_t CFSR = SCB->CFSR;
   // Hard Fault Status Register
@@ -82,14 +149,21 @@ void BusFault_Handler(void)
   //System Handler Control and State Register
   volatile uint32_t SHCSR = SCB->SHCSR;
 
-  for(;;)
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
   {
-
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
   }
 }
 
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
 void UsageFault_Handler(void)
 {
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
   // Configurable Fault Status Register
   volatile uint32_t CFSR = SCB->CFSR;
   // Hard Fault Status Register
@@ -105,38 +179,144 @@ void UsageFault_Handler(void)
   //System Handler Control and State Register
   volatile uint32_t SHCSR = SCB->SHCSR;
 
-  for(;;)
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
   {
-
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
   }
 }
 
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
 void DebugMon_Handler(void)
 {
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
 
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
 }
 
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32H7xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32h7xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles FDCAN1 interrupt 0.
+  */
 void FDCAN1_IT0_IRQHandler(void)
 {
+  /* USER CODE BEGIN FDCAN1_IT0_IRQn 0 */
+
+  /* USER CODE END FDCAN1_IT0_IRQn 0 */
   HAL_FDCAN_IRQHandler(&hfdcan1);
+  /* USER CODE BEGIN FDCAN1_IT0_IRQn 1 */
+
+  /* USER CODE END FDCAN1_IT0_IRQn 1 */
 }
 
+/**
+  * @brief This function handles FDCAN1 interrupt 1.
+  */
 void FDCAN1_IT1_IRQHandler(void)
 {
+  /* USER CODE BEGIN FDCAN1_IT1_IRQn 0 */
+
+  /* USER CODE END FDCAN1_IT1_IRQn 0 */
   HAL_FDCAN_IRQHandler(&hfdcan1);
+  /* USER CODE BEGIN FDCAN1_IT1_IRQn 1 */
+
+  /* USER CODE END FDCAN1_IT1_IRQn 1 */
 }
 
-void FDCAN_CAL_IRQHandler(void)
+/**
+  * @brief This function handles USART1 global interrupt.
+  */
+void USART1_IRQHandler(void)
 {
-  HAL_FDCAN_IRQHandler(&hfdcan1);
+  /* USER CODE BEGIN USART1_IRQn 0 */
+
+  /* USER CODE END USART1_IRQn 0 */
+  HAL_UART_IRQHandler(&huart1);
+  /* USER CODE BEGIN USART1_IRQn 1 */
+
+  /* USER CODE END USART1_IRQn 1 */
 }
 
-void QUADSPI_IRQHandler(void)
+/**
+  * @brief This function handles USB On The Go HS global interrupt.
+  */
+void OTG_HS_IRQHandler(void)
 {
-  HAL_QSPI_IRQHandler(&hqspi);
+  /* USER CODE BEGIN OTG_HS_IRQn 0 */
+
+  /* USER CODE END OTG_HS_IRQn 0 */
+  HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
+  /* USER CODE BEGIN OTG_HS_IRQn 1 */
+
+  /* USER CODE END OTG_HS_IRQn 1 */
 }
 
+/**
+  * @brief This function handles TIM17 global interrupt.
+  */
 void TIM17_IRQHandler(void)
 {
+  /* USER CODE BEGIN TIM17_IRQn 0 */
+
+  /* USER CODE END TIM17_IRQn 0 */
   HAL_TIM_IRQHandler(&htim17);
+  /* USER CODE BEGIN TIM17_IRQn 1 */
+
+  /* USER CODE END TIM17_IRQn 1 */
 }
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
